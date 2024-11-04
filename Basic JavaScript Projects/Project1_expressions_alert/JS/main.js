93b1181b20e9f828aa13598a0fcc635907cc27f1
the3zero3 <thedini23@gmail.com>
2024-11-04 12:27:49 -0800

    Added HTML and JS files

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+//Assigns two string variables
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string"
+
+//Concatenates and prints the provided arguements
+document.write(Sent1 + Sent2);
+
+//Displays a pop-up with the provided expression
+window.alert(1015 + 1009);
